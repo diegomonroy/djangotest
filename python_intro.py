79d4fbf2c7188a046d1397a8b15e48064ce65e61
Diego Fernando Monroy Munoz <dmsoftwaresas@gmail.com>
2016-10-06 16:09:59 -0400

    Examples in Python.

--- python_intro.py
+++ python_intro.py
@@ -1,4 +1,74 @@
+# Strings
+
+# >>> "Ola"
+# >>> "Hola " + "Ola"
+# >>> "Ola" * 3
+# >>> "Runnin' down the hill"
+# >>> 'Runnin\' down the hill'
+# >>> "Ola".upper()
+# >>> len("Ola")
+# >>> len(str(304023))
+
+# Variables
+
+# >>> name = "Ola"
+# >>> name
+# >>> len(name)
+# >>> a = 4
+# >>> b = 6
+# >>> a * b
+
+# Función print
+
+# >>> name = 'Maria'
+# >>> name
+# >>> print(name)
+
+# Listas
+
+# >>> lottery = [3, 42, 12, 19, 30, 59]
+# >>> len(lottery)
+# >>> lottery.sort()
+# >>> print(lottery)
+# >>> lottery.reverse()
+# >>> print(lottery)
+# >>> lottery.append(199)
+# >>> print(lottery)
+# >>> print(lottery[0])
+
+# Diccionarios
+
+# >>> participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+# >>> print(participant['name'])
+# >>> participant['favorite_language'] = 'Python'
+# >>> participant.pop('favorite_numbers')
+
+# Compara
+
+# >>> 5 > 2
+# >>> 3 < 1
+# >>> 5 > 2 * 2
+# >>> 1 == 1
+# >>> 5 != 2
+
+# >>> 6 >= 12 / 2
+# >>> 3 <= 2
+
+# >>> 6 > 2 and 2 < 3
+# >>> 3 > 2 and 2 < 1
+# >>> 3 > 2 or 2 < 1
+
+# Boolean
+
+# >>> a = True
+# >>> a
+
+# >>> a = 2 > 5
+# >>> a
+
 # print('Hello, Django test!')
+
+# If...elif...else
 
 # if 3 > 2:
 # 	print('It works!')
@@ -33,6 +103,8 @@
 # def hi(name):
 # 	print('Hi ' + name + '!')
 # hi("Rachel")
+
+# Bucles
 
 # def hi(name):
 # 	print('Hi ' + name + '!')
