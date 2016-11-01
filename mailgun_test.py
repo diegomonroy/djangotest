e90e3b54772e4c440712d6e22b26637c7925606b
Diego Fernando Monroy Munoz <dmsoftwaresas@gmail.com>
2016-11-01 11:15:58 -0400

    Update Mailgun example.

--- mailgun_test.py
+++ mailgun_test.py
@@ -1,12 +1,30 @@
+# Example 1
+
 import requests
 
 def send_simple_message():
     return requests.post(
-        "https://api.mailgun.net/v3/sandboxcc01264646364147b771514d6cc87e0f.mailgun.org/messages",
-        auth=("api", "key-c77133775487ac4908ae629cb028ed25"),
-        data={"from": "Mailgun Sandbox <postmaster@sandboxcc01264646364147b771514d6cc87e0f.mailgun.org>",
-              "to": "Diego Monroy <gn4@gaitannicholls.com>",
-              "subject": "Hello Diego Monroy",
-              "text": "Congratulations Diego Monroy, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."})
+        "https://api.mailgun.net/v3/domain.com/messages",
+        auth=("api", "key-5891601XXXXXXXXXXXXXXXf"),
+        data={"from": "Mailgun Sandbox <postmaster@domain.com>",
+              "to": "Customer 1 <customer1@gmail.com>",
+              "subject": "Hello Customer 1",
+              "text": "Congratulations Customer 1, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free."})
 
 send_simple_message()
+
+# Example 2
+
+import requests
+
+def send_simple_message(API_BASE_URL, API_KEY):
+    return requests.post(
+        API_BASE_URL,
+        auth=("api", API_KEY),
+        data={"from": "Developer contact@narenarya.in",
+              "to": ["customer1@gmail.com", "customer2@gmail.com"],
+              "subject": "Hello!",
+              "text": "Thanks for signing up with us!."})
+
+send_simple_message("https://api.mailgun.net/v3/narenarya.in/messages",
+                    "key-5891601XXXXXXXXXXXXXXXf")
